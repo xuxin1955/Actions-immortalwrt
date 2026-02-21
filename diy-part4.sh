a3cef904be1b023e580bed5410965d288a7801d2
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-02-21 10:56:29 +0800

    Comment out QModem and OpenClash git clone commands
    Comment out lines for QModem and OpenClash installation.

--- diy-part4.sh
+++ diy-part4.sh
@@ -5,10 +5,10 @@
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # mode
-echo 'src-git QModem https://github.com/FUjr/QModem' >> feeds.conf.default
+# echo 'src-git QModem https://github.com/FUjr/QModem' >> feeds.conf.default
 
 # OpenClash
-git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
+# git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
 
 # temp
