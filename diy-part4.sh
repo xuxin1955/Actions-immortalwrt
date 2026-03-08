b9993df8a5373e82823af9b04f7fa1be0b4f3668
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-09 06:32:32 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -11,9 +11,9 @@ echo 'src-git QModem https://github.com/FUjr/QModem' >> feeds.conf.default
 git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
 # temp
-git clone https://github.com/lkiuyu/luci-app-cpu-perf package/luci-app-cpu-perf
-git clone https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
-git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini package/luci-app-cpu-status-mini
+git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
 
 # packages
 sed -i 's|src-git packages https://github.com/immortalwrt/packages.git;openwrt-25.12|src-git packages https://github.com/xuxin1955/packages.git;openwrt-25.12|g' feeds.conf.default
