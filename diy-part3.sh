2a6e0791523dc630d92317d12dbb902dc949bd44
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-14 10:56:39 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -10,13 +10,11 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-### 添加第三方订阅源
-# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# hlk7628n dts
+# mkdir -p target/linux/ramips/dts/
+# cp -f "$GITHUB_WORKSPACE/dts/mt7628an_hilink_hlk-7628n.dts" target/linux/ramips/dts/mt7628an_hilink_hlk-7628n.dts
 
-# 自定义第三方插件
-# git clone https://github.com/chenmozhijin/luci-app-socat.git package/luci-app-socat
-# git clone https://github.com/qianlyun123/luci-app-modem.git package/luci-app-modem
-# git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
-# git clone https://github.com/linkease/istore.git package/istore
-# git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
+
+# turboacc
+# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
