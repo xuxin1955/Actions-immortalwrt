ed9ffc51b4cbb882eb8f3a97c6b38003228eda15
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-25 15:01:34 +0800

    Update diy-part6.sh

--- diy-part6.sh
+++ diy-part6.sh
@@ -9,6 +9,11 @@ cp -f "$GITHUB_WORKSPACE/scripts/dts/mt7628an_hilink_hlk-7628d.dts" "target/linu
 mkdir -p target/linux/ramips/image/
 cp -f "$GITHUB_WORKSPACE/scripts/image/mt76x8.mk" "target/linux/ramips/image/mt76x8.mk"
 
+# hlk7628dna board.d
+mkdir -p target/linux/ramips/mt76x8/base-files/etc/board.d && cp -f "$GITHUB_WORKSPACE/scripts/board.d/02_network" target/linux/ramips/mt76x8/base-files/etc/board.d/02_network
+
+
+
 
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
