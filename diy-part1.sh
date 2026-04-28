aabb7da9eb1cc062a748eb781f8a1221065616a2
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-28 18:47:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,7 @@
 
 
 # OpenClash
-git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
+# git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
 # turboacc
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
