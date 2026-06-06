9cc592ceff13ccb30fe6eeba52093a510c93b246
xuxin1955 <x2874836915@qq.com>
2026-06-06 16:35:24 +0800

    update

--- diy-part5.sh
+++ diy-part5.sh
@@ -7,7 +7,6 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 mkdir -p target/linux/ramips/dts/
 cp -f "$GITHUB_WORKSPACE/scripts/mt7620/mt7620a_zbtlink_zbt-we826-32m.dts" "target/linux/ramips/dts/mt7620a_zbtlink_zbt-we826-32m.dts"
 cp -f "$GITHUB_WORKSPACE/scripts/mt7620/mt7620a_glinet_gl-mt750.dts" "target/linux/ramips/dts/mt7620a_glinet_gl-mt750.dts"
-cp -f "$GITHUB_WORKSPACE/scripts/mt7620/mt7620a_xiaomi_miwifi-mini.dts" "target/linux/ramips/dts/mt7620a_xiaomi_miwifi-mini.dts"
 
 mkdir -p "target/linux/ramips/mt7620/base-files/etc/board.d/"
 cp -f "$GITHUB_WORKSPACE/scripts/mt7620/01_leds" "target/linux/ramips/mt7620/base-files/etc/board.d/01_leds"
